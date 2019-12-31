971b6d87d9943922d64d723af208ccde049cadf7
Till Wehowski <github@webfan.de>
2019-12-31 13:58:50 +0100

    Update setDirectory.php

--- config/setDirectory.php
+++ config/setDirectory.php
@@ -5,15 +5,19 @@ use function DI\decorate;
 
 return  [
 	
-		
-	'state.emitter' => decorate(function($emitter, ContainerInterface $c){
-	     $emitter->once('bootstrap', function($eventName, $emitter, $c){
-             $d = $c->get('project')->dir.\DIRECTORY_SEPARATOR.'compiled'.\DIRECTORY_SEPARATOR.'~events';
+	
+	'dir.compiled.events' =>function (ContainerInterface $c) {			                
+	     $d = $c->get('project')->dir.\DIRECTORY_SEPARATOR.'compiled'.\DIRECTORY_SEPARATOR.'~events';
              if(!is_dir($d)){
                mkdir($d, 0755, true);
              }
-             \Webfan\App\EventModule::setBaseDir($d);
-          });
+	  return $d;	
+        },	
+	
+	'state.emitter' => decorate(function($emitter, ContainerInterface $c){
+	   $emitter->once('bootstrap', function($eventName, $emitter, $container){
+                \Webfan\App\EventModule::setBaseDir($container->get('dir.compiled.events'));
+           });
 		return $emitter;
 	}),
 	
